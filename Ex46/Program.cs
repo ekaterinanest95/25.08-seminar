acbb27778002a0627477a44f33d9623d0e922ebd
Ekaterina <katya.kinal@gmail.com>
2022-08-25 20:27:37 +0300

    Ex46

---
+++ Ex46/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+/*m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1*/
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+// m- строчки, n - столбцы
+int[,] GetArray (int m, int n)
+{
+    int [,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(11);
+
+        }
+    }
+    return matrix;
+}
+
+int[,] result = GetArray(rows, columns);  //числа от 0 до 10 в двумерном массиве
+
+void PrintArray (int [,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(result);
+
+
+
