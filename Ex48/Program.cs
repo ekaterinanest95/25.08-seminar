035dccd15be273f68ffd189197f225d19e085a3c
Ekaterina <katya.kinal@gmail.com>
2022-08-25 21:39:59 +0300

    Ex49

---
+++ Ex48/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран. m = 3, n = 4.
+
+int rows = 3;
+
+int columns = 4;
+
+int[,] GetArray (int m, int n)
+{
+    int [,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+
+        }
+    }
+    return matrix;
+}
+
+int [,] array = GetArray(rows, columns);
+
+void PrintArray (int [,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
---
+++ Ex49/Program.cs
@@ -0,0 +1,41 @@
+﻿// 49. Задайте двумерный массив. 
+//Найдите элементы, у которых оба индекса чётные, 
+//и замените эти элементы на их квадраты.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+for (int i = 0; i < matrix.GetLength(0); i++) // строчки
+{
+    // j, m , k 
+    for (int j = 0; j < matrix.GetLength(1); j++) // cтолбцы
+    {
+        matrix[i, j] = new Random().Next(11); // [0;10]
+        Console.Write(matrix[i, j] + "\t"); // "\t"
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Result array:");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0)
+        {
+            Console.Write((matrix[i, j] * matrix[i, j] )+ "\t");
+        }
+        else Console.Write(matrix[i, j] + "\t"); // "\t"
+    }
+   Console.WriteLine();
+}
+
+
+
+
+
